c398c98c2a6f5669ae66dc51c27beb4534748fcd
fxdstudiokg <fxd@studiokg.fr>
2024-06-05 16:25:16 +0200

    Fix correction du systeme de routage

--- Js/Router/allRoutes.js
+++ Js/Router/allRoutes.js
@@ -1,8 +1,7 @@
 import Route from "./route.js";
 
-//Définir ici vos routes
 export const allRoutes = [
-  new Route("/", "Accueil", "/pages/home.html", []),
+  new Route("/", "Accueil", "./pages/home.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
--- Js/Router/router.js
+++ Js/Router/router.js
@@ -2,7 +2,7 @@ import Route from "./route.js";
 import { allRoutes, websiteName } from "./allRoutes.js";
 
 // Creating a route for the 404 page (page not found)
-const route404 = new Route("404", "Page introuvable", "/pages/404.html", []);
+const route404 = new Route("404", "Page introuvable", "/pages/404.html");
 
 // Function to retrieve the route corresponding to a given URL
 const getRouteByUrl = (url) => {
@@ -26,44 +26,24 @@ const LoadContentPage = async () => {
   const path = window.location.pathname;
   // Récupération de l'URL actuelle
   const actualRoute = getRouteByUrl(path);
-
   //Retrieving the current URL
-  const allRolesArray = actualRoute.authorize;
-
-  if(allRolesArray.length > 0) {
-    if(allRolesArray.includes("disconnected")){
-      if(isConnected()){
-        window.location.replace("/");
-      }
-    } else {
-      const roleUser = getRole();
-      if(!allRolesArray.includes(roleUser)){
-        window.location.replace("/");
-      } 
-    }
-  }
-
-  // Retrieving HTML content from the route
   const html = await fetch(actualRoute.pathHtml).then((data) => data.text());
   // Adding HTML content to element with id "main-page"
   document.getElementById("main-page").innerHTML = html;
 
   // Adding JavaScript content
   if (actualRoute.pathJS != "") {
-    // Creating a script tag
+    // Adding the script tag to the body of the document
     let scriptTag = document.createElement("script");
     scriptTag.setAttribute("type", "text/javascript");
     scriptTag.setAttribute("src", actualRoute.pathJS);
 
-    // Adding the script tag to the body of the document
+    // Changing the page title
     document.querySelector("body").appendChild(scriptTag);
   }
 
-  // Changing the page title
+  // Changement du titre de la page
   document.title = actualRoute.title + " - " + websiteName;
-
-  // Show and hide elements based on role
-  showAndHideElementsForRoles();
 };
 
 // Function to handle routing events (click on links)
