38a0d25830b0c66188c81c704b29ccc56cfd1e1f
fxdstudiokg <fxd@studiokg.fr>
2024-06-12 12:38:59 +0200

    Creation des pages account signin et signup

--- Js/Router/allRoutes.js
+++ Js/Router/allRoutes.js
@@ -1,7 +1,10 @@
 import Route from "./route.js";
 
 export const allRoutes = [
-  new Route("/", "Accueil", "./pages/home.html"),
+  new Route("/", "Accueil", "./pages/home.html", []),
+  new Route("/signin", "Connexion", "./pages/auth/signin.html", ["disconnected"], "/Js/auth/signin.js"),
+  new Route("/signup", "Inscription", "./pages/auth/signup.html", ["disconnected"], "/Js/auth/signup.js"),
+  new Route("/account", "Mon compte", "/pages/auth/account.html",["visitor", "admin", "Js/account.js"]),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
